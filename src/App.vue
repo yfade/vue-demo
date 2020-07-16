352bd01207412034c30c3f36da5a86606daabe8c
daibiao <db_work@126.com>
2020-07-17 00:05:30 +0800

    slot 插槽

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,16 @@
             <!--通过prop向子组件传递函数，适合多级-->
             <List :comments="comments" :deleteComment="deleteComment"></List>
         </div>
+
+        <!--测试插槽-->
+        <SlotTest>
+            <template v-slot:header>aaaaaaaaaaa</template>
+            <template v-slot:footer>ccc</template>
+            <template v-slot:main>bbb</template>
+            <p>ddddddd</p>
+            <p>eeeeeeeeeeeeeeee</p>
+        </SlotTest>
+
     </div>
 </template>
 
@@ -22,11 +32,12 @@
     import Add from './components/Add'
     import List from './components/List'
     import PubSub from 'pubsub-js'
+    import SlotTest from './components/SlotTest'
     //绑定事件监听---订阅消息
     //触发事件---发布消息
 
     export default {
-        components: {Add, List},
+        components: {Add, List,SlotTest},
         data() {
             return {
                 comments: [ //数据在那个组件，更新数据的函数就应该定义在哪个组件
---
+++ src/components/SlotTest.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div id="div1">
+            <slot name="header"></slot>
+        </div>
+        <div id="div2">
+            <slot name="main"></slot>
+        </div>
+        <div id="div4">
+            <slot></slot>
+        </div>
+        <div id="div3">
+            <slot name="footer"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style>
+
+</style>

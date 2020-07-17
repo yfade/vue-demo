2b0a151dd72a1a41dd8ee4e4cbd4499bcd6ebe6b
daibiao <db_work@126.com>
2020-07-17 23:44:14 +0800

    axios

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,30 @@
 <template>
     <div>
-        <header class="site-header jumbotron">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12">
-                        <h1>请发表对React的评论</h1>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <div class="container">
-            <!--通过自定义事件试子组件可以调用父组件函数-->
-            <Add @addComment="addComment"></Add>
-            <!--通过prop向子组件传递函数，适合多级-->
-            <List :comments="comments" :deleteComment="deleteComment"></List>
-        </div>
-
-        <!--测试插槽-->
-        <SlotTest>
-            <template v-slot:header>aaaaaaaaaaa</template>
-            <template v-slot:footer>ccc</template>
-            <template v-slot:main>bbb</template>
-            <p>ddddddd</p>
-            <p>eeeeeeeeeeeeeeee</p>
-        </SlotTest>
-
+        <p v-if="!repoUrl">loading...</p>
+        <p v-else>most star repo is <a :href="repoUrl">{{repoName}}</a></p>
     </div>
 </template>
 
 <script>
-    import Add from './components/Add'
-    import List from './components/List'
-    import PubSub from 'pubsub-js'
-    import SlotTest from './components/SlotTest'
-    //绑定事件监听---订阅消息
-    //触发事件---发布消息
-
+    import axios from 'axios'
     export default {
-        components: {Add, List,SlotTest},
+        name: 'App',
         data() {
             return {
-                comments: [ //数据在那个组件，更新数据的函数就应该定义在哪个组件
-                    {userName: 'Tom', comment: 'hello Jerry!'},
-                    {userName: 'Jerry', comment: 'hello Tom!'},
-                ]
-            }
-        },
-        methods: {
-            addComment(comment) {
-                this.comments.unshift(comment);
-            },
-            deleteComment(index) {
-                this.comments.splice(index, 1);
+                repoUrl: '',
+                repoName: ''
             }
         },
         mounted() {
-            PubSub.subscribe('testPubsub',function (msg, data) {
-                alert(msg+'------'+data)
+            const url = "https://api.github.com/search/repositories?q=vu&sort=stars";
+            axios.get(url).then(
+                response => {
+                    const item = response.data.items[0];
+                    this.repoUrl = item.owner.html_url;
+                    this.repoName = item.name;
+                }
+            ).catch(error => {
+                alert(error)
             })
         }
     }
